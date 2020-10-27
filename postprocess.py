56a957b926ffe5a1e21197fad114236a4500885a
安竹洋平 <61961825+yasutakeyohei@users.noreply.github.com>
2020-10-28 00:10:45 +0900

    mdのプロパティとhtmlに記載の公開日から、更新・公開日をhtmlに追加する機能を追加

--- postprocess.py
+++ postprocess.py
@@ -1,12 +1,44 @@
 import glob
 import re
 import shutil
+import os
+import datetime
+
+from datetime import datetime as dt
 
 # index.htmlの削除
 for filepath in glob.iglob('../../book/**/*.html', recursive=True):
+    filepath = os.path.normpath(filepath)
+    print(os.sep)
+    if(os.sep == "/") :
+        fp = re.sub(r"../../book/html/(.+)/.html", "\\1.md", filepath, 0)
+    else :
+        fp = re.sub(r"..\\..\\book\\html\\(.+)\.html", "\\1.md", filepath, 0)
+        fp = fp.replace("\\", "/")
+
+    githubp = "https://github.com/yasutakeyohei/Reiwa2/commits/master/src/" + fp
+    fp = "../../src/" + fp
+    # print(fp)
+    # print(githubp)
+
+    key = ""
+    if os.path.exists(fp) :
+        dt = datetime.datetime.fromtimestamp(os.stat(fp).st_mtime)
+        keyGTM = dt.strftime('%Y-%m-%dT%H:%M:%S+09:00')
+        key = dt.strftime('%Y-%m-%d')
+
     with open(filepath, encoding="utf8") as file:
         s = file.read()
-    s = re.sub(r"(href\s*=\s*\".*?)index.html(.*?\")", "\\1\\2", s, 0)
+    s = re.sub(r"(href\s*=\s*\".*?)index.html(.*?\")", "\\1\\2", s, 0) #index.htmlの削除
+    if key != "" :
+        replace = '''
+            <ul class="published-at-updated-at">
+                <li><a href="{githubp}"><i class="fa fa-refresh" aria-hidden="true" title="更新日"></i> <time datetime="{keyGTM}" timeprop="modified" title="更新日">{key}</a></time></li>
+                <li><i class="fa fa-file-text-o" aria-hidden="true" title="公開日"></i> <time datetime="\\1" timeprop="datepublished" title="公開日">\\1</time></li>
+            </ul>
+        '''.format(keyGTM = keyGTM, key = key, githubp = githubp)
+        s = re.sub(r"<p>.*{{first:(.*)}}.*</p>", replace, s, 0)
+    
     with open(filepath, "w", encoding="utf8") as file:
         file.write(s)
 
